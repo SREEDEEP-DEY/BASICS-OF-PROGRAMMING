9a524dc11c343a3e6ae134e5855dc86ae85725e9
SREEDEEP DEY <152058891+SREEDEEP-DEY@users.noreply.github.com>
2024-07-21 15:20:16 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The array is: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
